019cc61313482125c1f2b0b40d5e8bd075ccca88
FranciscoGontijo <franciscoacmg@gmail.com>
2024-02-08 17:50:40 -0300

    Add gifs, CSS

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const body = document.querySelector('#body');
 
 const title = document.createElement('h1');
+title.className = 'main-title';
 title.innerHTML = 'Wanna go out with me?';
 
 body.appendChild(title);
+
+const askingGif = document.createElement('img');
+askingGif.src = 'https://i.giphy.com/XCm6aoloWyUkwUuqkf.webp'
+askingGif.className = 'gif-embed';
+//<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/XCm6aoloWyUkwUuqkf" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/wiggle-vanlau-bunniekingdom-XCm6aoloWyUkwUuqkf">via GIPHY</a></p>
+//<iframe src="https://giphy.com/embed/XCm6aoloWyUkwUuqkf" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/wiggle-vanlau-bunniekingdom-XCm6aoloWyUkwUuqkf">via GIPHY</a></p>
+body.appendChild(askingGif);
 
 const yesButton = document.createElement('button');
 yesButton.className = 'yes-btn';
@@ -36,11 +44,22 @@ const acceptRequest = () => {
     body.removeChild(noButton);
     body.removeChild(yesButton);
     body.removeChild(title);
+    body.removeChild(askingGif);
 
     const yayText = document.createElement('h1');
+    yayText.style.color = 'gray';
+    yayText.style.fontSize = '38px';
+    yayText.style.fontFamily = 'sans-serif';
+    yayText.style.fontWeight = 'bold';
+    yayText.style.textAlign = 'center';
     yayText.innerHTML = 'Yay, pick you up at 8pm';
 
+    const yayGif = document.createElement('img');
+    yayGif.src = 'https://i.giphy.com/MeIucAjPKoA120R7sN.webp'
+    yayGif.className = 'gif-embed';
+
     body.appendChild(yayText);
+    body.appendChild(yayGif);
 }
 
 yesButton.addEventListener('mouseover', growButton);
